4c8a31f2677c048a793d1f9aeecd9c8d487667b2
Simon Flueckiger <si.flueckiger@gmail.com>
2019-08-15 13:21:30 +0100

    correct script ..Stream.sh

--- cStream.sh
+++ cStream.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd
+cd Desktop/mjpg-streamer/mjpg-streamer-experimental/
 
 export LD_LIBRARY_PATH=.
 ./mjpg_streamer -o "output_http.so -p 7070 -w ./www" -i "input_raspicam.so -x 400 -y 480 -fps 20 -ex night"
