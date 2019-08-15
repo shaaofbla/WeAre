13fa1fbe576fc1a568ea36f065bb46ff74368f66
Simon Flueckiger <si.flueckiger@gmail.com>
2019-08-15 19:01:03 +0200

    changes

--- aProcessing.sh
+++ aProcessing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export DISPLAY=:0; processing-java --sketch=WeAre_MJPEG_Stream/ --run A
+export DISPLAY=:0; processing-java --sketch=WeAre_MJPEG_Stream/ --run B
 
 echo "Sketch for camera A is running."
--- bProcessing.sh
+++ bProcessing.sh
@@ -2,6 +2,6 @@
 cd
 cd WeAre/
 
-export DISPLAY=:0; processing-java --sketch=WeAre_MJPEG_Stream/ --run B
+export DISPLAY=:0; processing-java --sketch=WeAre_MJPEG_Stream/ --run A
 
 echo "Sketch for camera B is running."
